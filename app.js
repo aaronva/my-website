5a2c686437f77089d360c4a55a4b198f7005173e
A Vaartjes <aaron.va@gmail.com>
2016-06-14 00:30:42 -0400

    Deploy

--- app.js
+++ app.js
@@ -3,29 +3,41 @@
  */
 
 var app = angular.module('app', []);
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/about-me', {
+            templateUrl: 'about-me/about-me.html',
+            // controller: 'AboutMeController',
+        })
+        .when('/portfolio', {
+            templateUrl: 'portfolio/portfolio.html',
+            // controller: 'PortfolioController'
+        });
+});
+
 app.controller('main', function($scope, globals) {
     // Controller stuff
-    $scope.globals = globals;
+    // $scope.globals = globals;
 });
-app.controller('navBar', function($scope, globals) {
-    // Controller stuff
-    var pageLookup = {
-        aboutMe: {
-            templateUrl: 'about-me/about-me.html'
-        },
-        portfolio: {
-            templateUrl: 'portfolio/portfolio.html'
-        }
-    };
-
-    $scope.setCurPage = function (current) {
-        globals.activePage = pageLookup[current];
-    }
-});
-app.factory('globals', function() {
-    return {
-        activePage: {
-            templateUrl: 'about-me/about-me.html'
-        }
-    }
-});
+// app.controller('navBar', function($scope, globals) {
+//     // Controller stuff
+//     var pageLookup = {
+//         aboutMe: {
+//             templateUrl: 'about-me/about-me.html'
+//         },
+//         portfolio: {
+//             templateUrl: 'portfolio/portfolio.html'
+//         }
+//     };
+//
+//     $scope.setCurPage = function (current) {
+//         globals.activePage = pageLookup[current];
+//     }
+// });
+// app.factory('globals', function() {
+//     return {
+//         activePage: {
+//             templateUrl: 'about-me/about-me.html'
+//         }
+//     }
+// });
