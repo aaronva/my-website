80b7b2b476a4ee66f3c3f116ad843dc7f629c1a0
A Vaartjes <aaron.va@gmail.com>
2016-06-14 01:08:15 -0400

    Deploy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = angular.module('app', ['ngRoute']);
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
         .when('/about-me', {
-            templateUrl: 'about-me-1/about-me.html',
+            templateUrl: 'about-me/about-me.html',
             controller: 'AboutMeController'
         })
         .when('/portfolio', {
@@ -20,30 +20,10 @@ app.controller('main', function($scope) {
     // Controller stuff
     // $scope.globals = globals;
 });
+app.controller('NavBarController', function ($scope, $route) {
+    console.log($route.current);
+});
 app.controller('AboutMeController', function($scope) {
 });
 app.controller('PortfolioController', function($scope) {
 });
-
-// app.controller('navBar', function($scope, globals) {
-//     // Controller stuff
-//     var pageLookup = {
-//         aboutMe: {
-//             templateUrl: 'about-me-1/about-me-1.html'
-//         },
-//         portfolio: {
-//             templateUrl: 'portfolio/portfolio.html'
-//         }
-//     };
-//
-//     $scope.setCurPage = function (current) {
-//         globals.activePage = pageLookup[current];
-//     }
-// });
-// app.factory('globals', function() {
-//     return {
-//         activePage: {
-//             templateUrl: 'about-me-1/about-me-1.html'
-//         }
-//     }
-// });
