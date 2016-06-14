e7226842fac9573f6ee0864b403832c57bad349c
A Vaartjes <aaron.va@gmail.com>
2016-06-14 01:39:31 -0400

    Added HTML5 url routing and staates

--- app.js
+++ app.js
@@ -2,10 +2,32 @@
  * Created by aaron on 2016-05-23.
  */
 
-    var app = angular.module('app', []);
-    app.controller('main', function($scope) {
-        // Controller stuff
-        $scope.activePage = {
+var app = angular.module('app', ['ui.router']);
+app.config(function($stateProvider, $locationProvider) {
+    $stateProvider
+        .state('about-me', {
+            url: "/about-me",
             templateUrl: 'about-me/about-me.html'
-        }
-    });
+        })
+        .state('portfolio', {
+            url: "/portfolio",
+            templateUrl: "portfolio/portfolio.html"
+        });
+
+
+    $locationProvider.html5Mode(true);
+});
+app.controller('main', function($scope) {
+    // Controller stuff
+    // $scope.globals = globals;
+});
+app.controller('NavBarController', function ($scope, $state) {
+    $scope.selected = function(state) {
+        return state === $state.current.name;
+        console.log($state.current);
+    };
+});
+app.controller('AboutMeController', function($scope) {
+});
+app.controller('PortfolioController', function($scope) {
+});
