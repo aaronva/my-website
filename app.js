fd5fdecf40a1636b013b9cf8f372be12b275d0ce
A Vaartjes <aaron.va@gmail.com>
2016-06-14 00:11:25 -0400

    Deploy

--- app.js
+++ app.js
@@ -2,14 +2,18 @@
  * Created by aaron on 2016-05-23.
  */
 
-    var app = angular.module('app', []);
-    app.controller('main', function($scope, global) {
-        // Controller stuff
-        $scope.activePage = global.activePage;
-    });
-    app.factory('global', function() {
-        this.activePage = {
-            templateUrl: 'about-me/about-me.html'
-        }
-    });
+var app = angular.module('app', []);
+app.controller('main', function($scope, activePage) {
+    // Controller stuff
+    $scope.activePage = activePage;
 
+    activePage = {
+        templateUrl: 'about-me/about-me.html'
+    }
+});
+app.factory('activePage', function() {
+    return {
+        // templateUrl: 'about-me/about-me.html'
+        // templateUrl: 'sidebar/sidebar.html'
+    }
+});
